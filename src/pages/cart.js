6b41642b9c01f812d7df96883ba87c899294ad9a
Dane Maison <danemaison@gmail.com>
2019-12-21 14:53:12 -0800

    Added cart page

---
+++ src/pages/cart.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import {Container, Header} from '../components/ui/elements';
+import styled from 'styled-components';
+const CartContainer = styled(Container)`
+  padding-top:25px;
+`
+
+const Cart = ()=>{
+  return (
+    <Layout>
+      <SEO title="Cart" />
+      <CartContainer>
+        <Header>Items in Your Cart</Header>
+      </CartContainer>
+    </Layout>
+  )
+}
+
+export default Cart;
