7ce23604fcf2a257827542ac959b88b8b70168b6
Dane Maison <danemaison@gmail.com>
2019-12-26 14:38:56 -0800

    Added styling to product page

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -6,6 +6,9 @@ import { graphql } from "gatsby"
 import Image from "gatsby-image"
 import StoreContext from "../provider/context"
 import ProductForm from './product-page-form';
+import { Link } from "gatsby"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons"
 
 const ProductPageContainer = styled(Container)`
   padding-top: 25px;
@@ -28,7 +31,7 @@ const Description = styled.div`
   }
 `
 const Row = styled.div`
-  padding-top: 30px;
+  padding-top: 10px;
   display: flex;
   align-items: center;
   flex-direction: column;
@@ -40,39 +43,70 @@ const Row = styled.div`
 
   @media ${({ theme }) => theme.mediaQueries.medium} {
     flex-direction: row;
+    align-items: flex-start;
+    width: 760px;
+  }
+  @media ${({ theme }) => theme.mediaQueries.large} {
+    width: 900px;
+  }
+  @media ${({ theme }) => theme.mediaQueries.largest} {
+    width: 1080px;
   }
 `
 
 const Col = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: flex-start;
+  align-items: flex-start;
   flex-direction: column;
 `
 const ImageCol = styled(Col)`
-  margin-top:15px;
-  align-self:flex-start;
+align-items:center;
+width:100%;
+  margin-top: 15px;
+  align-self: flex-start;
+  @media ${({ theme }) => theme.mediaQueries.medium} {
+    flex-direction: row-reverse;
+  }
 `
 
 const AltImagesWrapper = styled.div`
-  display:flex;
-  justify-content:flex-start;
-  flex-wrap:wrap;
-  width:100%;
-  align-items:center;
+  margin: 20px 0;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  /* width: 100%; */
+  align-items: center;
+  @media ${({ theme }) => theme.mediaQueries.medium} {
+    flex-direction: column;
+  }
 `
 const AltImgWrapper = styled.div`
   margin: 10px 10px 0 0;
-  width: 125px;
-  height: 125px;
-  border:1px solid ${({theme})=>theme.grey};
+  width: 75px;
+  height: 75px;
+  /* border:1px solid ${({theme})=>theme.grey}; */
   /* border-radius:5px; */
-  /* box-shadow: 0 0 2px rgba(0,0,0,.25); */
+  box-shadow: 0 0 2px rgba(0,0,0,.2);
 `
 const AltImg = styled(Image)`
   max-width:100%;
   max-height:100%;
 `
+
+const BackRow = styled(Row)`
+  font-size: 0.8rem;
+  margin: 0;
+  padding-top: 16px;
+  padding-bottom: 0px;
+  justify-content: flex-start;
+  align-items: flex-start;
+`
+const BackToShopWrapper = styled(Link)`
+  text-decoration: none;
+  color: black;
+`
+const BackArrow = styled(FontAwesomeIcon)``
 
 const ProductPage = ({ data }) => {
   const { title, images, descriptionHtml } = data.shopifyProduct
@@ -82,27 +116,36 @@ const ProductPage = ({ data }) => {
     <Layout>
       <ProductPageContainer>
         <Header>{title}</Header>
+        <BackRow>
+          <BackToShopWrapper to="/shop">
+            <BackArrow icon={faArrowLeft} /> Back to shop
+          </BackToShopWrapper>
+        </BackRow>
         <Row>
-          <ImageCol>
-            <ImageWrapper>
-              <Img
-                fluid={images[0].localFile.childImageSharp.fluid}
-                imgStyle={{ objectFit: "contain" }}
-              />
-            </ImageWrapper>
-            <AltImagesWrapper>
-              {images.map(img => (
-                <AltImgWrapper>
-                  <AltImg imgStyle={{objectFit: "contain"}} fluid={img.localFile.childImageSharp.fluid} />
-                </AltImgWrapper>
-              ))}
-            </AltImagesWrapper>
-          </ImageCol>
+          <Col>
+            <ImageCol>
+              <ImageWrapper>
+                <Img
+                  fluid={images[0].localFile.childImageSharp.fluid}
+                  imgStyle={{ objectFit: "contain" }}
+                />
+              </ImageWrapper>
+              <AltImagesWrapper>
+                {images.map(img => (
+                  <AltImgWrapper>
+                    <AltImg
+                      imgStyle={{ objectFit: "contain" }}
+                      fluid={img.localFile.childImageSharp.fluid}
+                    />
+                  </AltImgWrapper>
+                ))}
+              </AltImagesWrapper>
+            </ImageCol>
+            <ProductForm product={product} />
+          </Col>
           <Description dangerouslySetInnerHTML={{ __html: descriptionHtml }} />
         </Row>
-        <Row>
-          <ProductForm product={product} />
-        </Row>
+        <Row></Row>
       </ProductPageContainer>
     </Layout>
   )
